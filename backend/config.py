d2894e346c770761eb9d84906dac52993077bf19
SHAKTHI.K <ksakthi960@gmail.com>
2026-06-25 11:15:55 +0530

    Add files via upload

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ class Config:
     JWT_SECRET_KEY = "twite_jwt_secret"
 
     SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:Kshakthi960%40%23@localhost/attendance_db"
+        "mysql+pymysql://kvvsh1:Kshakthi960%40%23@kvvsh1.mysql.database.azure.com:3306/attendance_db?charset=utf8mb4"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
