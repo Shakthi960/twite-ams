b1ff114f8e314bf13382e2007e3bf460640dbea7
SHAKTHI.K <ksakthi960@gmail.com>
2026-06-25 11:21:26 +0530

    Add files via upload

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 class Config:
 
     SECRET_KEY = "twite_secret_key"
@@ -5,7 +7,9 @@ class Config:
     JWT_SECRET_KEY = "twite_jwt_secret"
 
     SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://kvvsh1:Kshakthi960%40%23@kvvsh1.mysql.database.azure.com:3306/attendance_db?charset=utf8mb4"
+        "mysql+pymysql://kvvsh1:Kshakthi960%40%23@kvvsh1.mysql.database.azure.com:3306/attendance_db?ssl_ca=ssl"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=12)
