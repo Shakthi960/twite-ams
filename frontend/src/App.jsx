fb41299d0c77ea5908ee32fedc90be5036adac7a
SHAKTHI.K <ksakthi960@gmail.com>
2026-06-25 22:40:45 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,35 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import Login from "./pages/login";
-// import Dashboard from "./pages/dashboard";
-// import Employees from "./pages/employees";
-// import Attendance from "./pages/attendance";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={<Login />}
-//         />
-
-//         <Route
-//           path="/dashboard"
-//           element={<Dashboard />}
-//         />
-
-//         <Route
-//           path="/employees"
-//           element={<Employees />}
-//         />
-
-//         <Route
-//           path="/attendance"
-//           element={<Attendance />}
-//         />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Employees from "./pages/employees";
+import Attendance from "./pages/attendance";
 
 function App() {
   return (
-    <h1
-      style={{
-        color: "red",
-        fontSize: "50px",
-        textAlign: "center",
-        marginTop: "100px"
-      }}
-    >
-      APP IS WORKING
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Login />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+        <Route
+          path="/employees"
+          element={<Employees />}
+        />
+
+        <Route
+          path="/attendance"
+          element={<Attendance />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
