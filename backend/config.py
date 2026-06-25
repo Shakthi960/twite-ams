4d227559afe42c86971dbbc8e9167421314f1116
SHAKTHI.K <ksakthi960@gmail.com>
2026-06-25 11:39:15 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -11,7 +11,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://kvvsh1:{password}"
         "@kvvsh1.mysql.database.azure.com:3306/attendance_db"
-        "?ssl_mode=REQUIRED"
+        "?ssl_mode=REQUIRED&charset=utf8mb4"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
