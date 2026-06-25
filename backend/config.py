fee2ba9f400084fe10e705810d802e8904b41a9c
SHAKTHI.K <ksakthi960@gmail.com>
2026-06-25 19:44:00 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -12,5 +12,20 @@ print("=" * 60)
 password = quote_plus(os.getenv("DB_PASSWORD"))
 
 print(
-    f"mysql+pymysql://{os.getenv('DB_USER')}:{password}@{os.getenv('DB_HOST')}:3306/{os.getenv('DB_NAME')}"
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{password}"
+    f"@{os.getenv('DB_HOST')}:3306/{os.getenv('DB_NAME')}"
 )
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://"
+        f"{os.getenv('DB_USER')}:{password}"
+        f"@{os.getenv('DB_HOST')}:3306/"
+        f"{os.getenv('DB_NAME')}"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=12)
